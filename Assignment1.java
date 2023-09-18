add3114e4d99436c69c1060332f2a15237fa7799
jgoldstei@hartford.edu <jgoldstei@hartford.edu>
2023-09-18 12:28:14 -0400

    Add string to int func

--- Assignment1.java
+++ Assignment1.java
@@ -1,11 +1,28 @@
 // Import libraries here
+import java.lang.Math;
 // Jonathan Goldstein
 
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
-    System.out.print("Please enter a 5-character string:\n");
-    var instring = System.console().readLine();
+    var inString = System.console().readLine("Please enter a 5-character string:\n");
+    var Fahrenheit = System.console().readLine("Enter Fahrenheit value:\n");
     
+    int out = SToInt(Fahrenheit);
+    System.console().printf("%d", out);
+  }
+
+
+
+  //Convert a string to an integer
+  static int SToInt(String in){
+    //variable to store final int
+    int final_val = 0;
+
+    for (int i = 0; i < in.length(); i++){
+      //Get current int in string, then multiply by its ten's power
+      final_val += ( (in.charAt(i)-48) * (Math.pow(10, in.length()-(i+1))) );
+    }
+    return final_val;
   }
 }
