aa3e9fc72b54ee1cea9cfe153ea09342aad5572c
jgoldstei@hartford.edu <jgoldstei@hartford.edu>
2023-09-18 12:47:42 -0400

    change Fahrenheit to Celsius and Gen final string

--- Assignment1.java
+++ Assignment1.java
@@ -1,15 +1,40 @@
 // Import libraries here
 import java.lang.Math;
+import java.util.Random;
 // Jonathan Goldstein
 
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+    int randnum = 0;
+    Random randgen = new Random();
+
+    var invString = "";
+    var finalString = "";
+    
+    float Celsius = 0;
+    //get user values
     var inString = System.console().readLine("Please enter a 5-character string:\n");
     var Fahrenheit = System.console().readLine("Enter Fahrenheit value:\n");
     
+    //get int version of Fahrenheit
     int out = SToInt(Fahrenheit);
-    System.console().printf("%d", out);
+    
+    //convert Fahrenheit to Celsius
+    Celsius = ((out - 32) * 5) / 9;
+
+    //invert inString
+    for (int i = inString.length(); i > 0; i--){
+      invString = invString.concat(inString.substring(i-1, i));
+    }
+    //remove first and last char
+    invString = invString.substring(1, invString.length()-1);
+
+    //generate a random number between 32 & 16384 (Inclusive)
+    randnum = randgen.nextInt(32, 16385);
+    
+    //Generate final string
+    finalString = finalString.concat(String.valueOf(Celsius).concat(invString.concat(String.valueOf(randnum))));
+    System.console().printf(finalString);
   }
 
 
